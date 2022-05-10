7f786a7b3f523782f3b6632141a84b3a41a41757
spikeCifer <stelioszapp+dev@gmail.com>
2022-05-10 11:14:42 +0300

    Some HTML Formatting

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 A Spring Boot Web App used for visualizing data collected from [The World Bank](https://data.worldbank.org/country).
 
-## Techologies Used
+## Technologies Used
 
 - Thymeleaf
 - Spring
 - MySQL
+
+## Installation Process
+In the case that the import scripts indicate that you can not import data to your
+MySQL server, check which directory has permission to do so and move the data files
+in that directory.
--- src/main/java/uoi/DataVisualizer/DataVisualizerApplication.java
+++ src/main/java/uoi/DataVisualizer/DataVisualizerApplication.java
@@ -1,18 +1,10 @@
 package uoi.DataVisualizer;
 
-import uoi.DataVisualizer.models.Indicator;
-
-import uoi.DataVisualizer.resositories.IndicatorRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DataVisualizerApplication {
-	private static final Logger log = LoggerFactory.getLogger(DataVisualizerApplication.class);
 	public static void main(String[] args) {
 
 		SpringApplication.run(DataVisualizerApplication.class, args);
