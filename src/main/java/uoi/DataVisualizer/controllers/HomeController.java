1063f32218721644a0c197bec1a33a59908b502e
spikeCifer <stelioszapp+dev@gmail.com>
2022-05-11 18:07:17 +0300

    Form gets processed

--- src/main/java/uoi/DataVisualizer/controllers/HomeController.java
+++ src/main/java/uoi/DataVisualizer/controllers/HomeController.java
@@ -1,14 +1,18 @@
 package uoi.DataVisualizer.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import uoi.DataVisualizer.models.Request;
 import uoi.DataVisualizer.resositories.CountryRepository;
 import uoi.DataVisualizer.resositories.IndicatorRepository;
 import uoi.DataVisualizer.resositories.TimePeriodRepository;
 
 
 @Controller
+@Slf4j
 public class HomeController {
     private final CountryRepository countryRepo;
     private final IndicatorRepository indicatorRepo;
@@ -26,5 +30,11 @@ public class HomeController {
         model.addAttribute("indicators",indicatorRepo.findAll());
         model.addAttribute("years", timeRepo.findAll());
         return "home";
+    }
+
+    @PostMapping("/visualize")
+    public String processSubmittedForm(Request request) {
+        log.info(request.toString());
+        return "result";
     }
 }
